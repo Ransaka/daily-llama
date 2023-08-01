9450ce0555d1dbdb990ae7bac80fe92150f37276
Ransaka <ransakaravi@icloud.com>
2023-08-01 16:11:20 +0530

    Finalized all modules

--- daily-llama.py
+++ daily-llama.py
@@ -1,5 +1,50 @@
 from model.generator import DailyLLAMA
+from typing import Any, Optional, Required
+from dataclasses import dataclass, field
+from transformers import HfArgumentParser
 
-model  = DailyLLAMA(source_data_path='/home/ransaka/daily-llama/data/news-small.json', source_column='title', content_column='content')
-resp = model("What happened to rupee in this week?")
-print(resp)
+@dataclass
+class ScriptArguments:
+    """
+    The name of the Casual LM model we wish to fine with SFTTrainer
+    """
+
+    model_name: Optional[str] = field(default="meta-llama/Llama-2-7b-chat-hf", metadata={"help": "the model name"})
+    dataset_path: Optional[str] = field(
+        default="data/news-small.json", metadata={"help": "the dataset location"}
+    )
+    embedding_model: Required[str] = field(
+        default="intfloat/e5-small-v2",metadata={"help": "the embedding model name"}
+    )
+    embedding_col: Optional[str] = field(default="title", metadata={"help": "the field name to embed"})
+    content_col: Optional[str] = field(default="content", metadata={"help": "the field name contains the content"})
+    load_in_8bit: Optional[bool] = field(default=False, metadata={"help": "load the model in 8 bits precision"})
+    load_in_4bit: Optional[bool] = field(default=False, metadata={"help": "load the model in 4 bits precision"})
+    trust_remote_code: Optional[bool] = field(default=True, metadata={"help": "Enable `trust_remote_code`"})
+    use_auth_token: Optional[bool] = field(default=True, metadata={"help": "Use HF auth token to access the model"})
+
+def generate():
+    args = HfArgumentParser(ScriptArguments).parse_args_into_dataclasses()[0]
+    daily_llama = DailyLLAMA(
+        source_data_path=args.dataset_path,
+        source_column=args.embedding_col,
+        content_column=args.content_col,
+        load_in_4bit=args.load_in_4bit,
+        load_in_8bit=args.load_in_8bit,
+        trust_remote_code=args.trust_remote_code,
+        model_name=args.model_name,
+        use_auth_token=args.use_auth_token,
+        embedding_model=args.embedding_model,
+    )
+
+    try:
+        while True:
+            user_input = input("Input: ")
+            response = daily_llama(user_input)
+            print(response)
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+
+if __name__ == "__main__":
+    generate()
--- model/generator.py
+++ model/generator.py
@@ -1,17 +1,71 @@
-from typing import Any, List
-
+from typing import Any, Optional
+from dataclasses import dataclass, field
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
 import numpy as np
+import torch
 from index.indexer import DailyLlamaIndexer
 from vector.vectorize import DailyLlamaVectorizer
 
 
 class DailyLLAMA:
-    def __init__(self, source_data_path, source_column, content_column, chat_model, embedding_model="intfloat/e5-small-v2") -> None:
+    def __init__(
+            self,
+            source_data_path,
+            source_column,
+            content_column,
+            load_in_4bit,
+            load_in_8bit,
+            trust_remote_code,
+            model_name,
+            use_auth_token,
+            embedding_model="intfloat/e5-small-v2",
+    ) -> None:
         self.vectorizer = DailyLlamaVectorizer(
             file_path=source_data_path, column_to_embed=source_column, content_column=content_column, model_id=embedding_model)
         self.embeddings = self.vectorizer.retrave_embeddings(
             output_type='numpy')
         self.indexer = DailyLlamaIndexer(self.embeddings)
+        self.load_in_4bit = load_in_4bit
+        self.load_in_8bit = load_in_8bit
+        self.trust_remote_code = trust_remote_code
+        self.model_name = model_name
+        self.use_auth_token = use_auth_token
+        self._load_model()
+
+    def configure_model_settings(self):
+        """
+        Configures the model settings based on the values of `load_in_8bit` and `load_in_4bit`.
+
+        Raises a ValueError if both `load_in_8bit` and `load_in_4bit` are True.
+
+        Sets the `quantization_config` based on the values of `load_in_8bit` and `load_in_4bit`.
+
+        Sets the `device_map` to {"": 0} and the `torch_dtype` to `torch.bfloat16` if either `load_in_8bit` or `load_in_4bit` is True.
+
+        Sets the `device_map`, `quantization_config`, and `torch_dtype` to None if neither `load_in_8bit` nor `load_in_4bit` is True.
+        """
+        # Check if both load_in_8bit and load_in_4bit are True
+        if self.load_in_8bit and self.load_in_4bit:
+            # Raise an error if both load_in_8bit and load_in_4bit are True
+            raise ValueError("You can't load the model in 8 bits and 4 bits at the same time")
+        # Check if either load_in_8bit or load_in_4bit is True
+        elif self.load_in_8bit or self.load_in_4bit:
+            # Configure quantization settings based on load_in_8bit and load_in_4bit values
+            self.quantization_config = BitsAndBytesConfig(
+                load_in_8bit=self.load_in_8bit, load_in_4bit=self.load_in_4bit
+            )
+            # Set device map with an empty string as key and 0 as value
+            self.device_map = {"": 0}
+            # Set torch data type to torch.bfloat16
+            self.torch_dtype = torch.bfloat16
+        # If neither load_in_8bit nor load_in_4bit is True
+        else:
+            # Set device map to None
+            self.device_map = None
+            # Set quantization config to None
+            self.quantization_config = None
+            # Set torch data type to None
+            self.torch_dtype = None
 
     def __call__(self, query, k=4) -> Any:
         """
@@ -28,7 +82,10 @@ class DailyLLAMA:
         topk = self.indexer.topk(vector=vector['embeddings'], k=k)
         docs = self.vectorizer.content[topk]
         docs = np.array(docs).reshape(-1)
-        return self.generate_prompt(docs=docs, query=query)
+        prompt =  self.generate_prompt(docs=docs, query=query)
+        response = self.generate(prompt=prompt)
+        assistance_response = response.split("ASSISTANT:")[-1].strip()
+        return assistance_response
 
     @staticmethod
     def generate_prompt(docs: np.array, query: str) -> str:
@@ -46,6 +103,65 @@ class DailyLLAMA:
                 "Answer user queries based on the below information. " \
                 "Start your answer with 'Based on past newspaper contents...'"
         information = "\n".join(docs)
-        return f"{intro}\n- {information}\n{query}"
-    
-    
+        # return f"{intro}\n- {information}\n{query}"
+        prompt_template = f'''SYSTEM: {intro}.
+        SYSTEM: {information}.
+                            USER: {query}
+
+                            ASSISTANT:
+                            '''
+        return prompt_template
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def _load_model(self):
+        """
+        Load the model and tokenizer for the chatbot.
+        
+        This function is responsible for configuring the model settings, loading the pre-trained model, and loading the tokenizer.
+        
+        Parameters:
+            self (object): The instance of the class.
+        
+        Returns:
+            None
+        """
+        # Configure model settings
+        self.configure_model_settings()
+        
+        # Load the model
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_name,  # Name of the pre-trained model
+            quantization_config=self.quantization_config,  # Quantization configuration
+            device_map=self.device_map,  # Device mapping
+            trust_remote_code=self.trust_remote_code,  # Whether to trust remote code
+            torch_dtype=self.torch_dtype,  # Torch data type
+            use_auth_token=self.use_auth_token,  # Whether to use authentication token
+        )
+        
+        # Load the tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+    def generate(self, prompt: str) -> str:
+        """
+        Generate a response using a given prompt.
+
+        Args:
+            prompt (str): The prompt to generate a response for.
+
+        Returns:
+            str: The generated response.
+        """
+        # Tokenize the prompt using the tokenizer and convert to PyTorch tensors
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
+        
+        # Disable gradient calculation and run the model to generate output
+        with torch.no_grad():
+            outputs = self.model.generate(**inputs)
+        
+        # Decode the generated output and remove special tokens
+        response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        
+        # Return the generated response
+        return response
